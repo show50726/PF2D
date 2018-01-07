1f87f16157cd526d8bf140cc0474842d4b6f81ac
samuelchou <samuelchou@hotmail.com.tw>
2018-01-07 23:04:11 +0800

    實作水的浮力。

---
+++ Assets/Scripts/Objects/Water.cs
@@ -0,0 +1,27 @@
+﻿//Water made by STC
+//contact:          stc.ntu@gmail.com
+//last maintained:  2018/01/07
+//Usage:            This is a simplified water, giving force to whom getting in.
+//NOTE:             set collider to trigger. 2D only.
+
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+
+    public float density = 1f;
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        Rigidbody2D objRb2d = col.GetComponent<Rigidbody2D>();
+        if (objRb2d == null) return;
+        float area = 1;
+        BoxCollider2D objBCol2d = col.GetComponent<BoxCollider2D>();
+        if (objBCol2d != null)
+        {
+            area = objBCol2d.size.x * objBCol2d.size.y;
+        }
+        objRb2d.AddForce(new Vector2(0, area * density));
+    }
+
+}
