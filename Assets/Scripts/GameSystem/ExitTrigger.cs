633bc61125c556ee89fcef7e4c778267cfba87b3
王小明 <k40526@hotmail.com>
2018-04-04 23:34:59 +0800

    結束遊戲功能

---
+++ Assets/Scripts/GameSystem/ExitTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExitTrigger : MonoBehaviour {
+
+    public GameSystemManager GSM;
+
+    [SerializeField]
+    private UnityEvent EndGame;
+
+    public void End()
+    {
+        this.EndGame.Invoke();
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+            End();
+    }
+
+
+}
