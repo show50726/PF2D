9e287fb58a44f768595a430291a83133474e3984
samuelchou <samuelchou@hotmail.com.tw>
2018-07-10 23:07:06 +0800

    功能升級：提供Return Area，現在可以環繞關卡周圍、避免玩家飛出關卡外了。

---
+++ Assets/Scripts/Objects/ReturnArea.cs
@@ -0,0 +1,70 @@
+﻿//ReturnArea made by STC
+//contact:          stc.ntu@gmail.com
+//last maintained:  2018/07/10
+//usage:            in some game -- like platformer -- in case the player go "too far," use this surround the level and the player would return.
+
+using UnityEngine;
+using UnityExtension;
+
+public class ReturnArea : STCMonoBehaviour
+{
+    public LayerMask _IgnoreTheseLayers = (1 << 1) | (1 << 2) | (1 << 4) | (1 << 5);
+    [Tooltip("portal anything who touched this to this point.")]
+    public Vector3 destination;
+
+    [Tooltip("If true, will zero velocity if there's rigidbody attached on objects.")]
+    public bool stopRigidbody = true;
+
+    private void OnEnable()
+    {
+        if (GetComponent<Collider>() == null && GetComponent<Collider2D>() == null)
+        {
+            DebugMessage(LogType.Error, "no trigger is set. Component will not work.");
+            enabled = false;
+            return;
+        }
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        TryPortal(col.gameObject, destination);
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        TryPortal(col.gameObject, destination);
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        TryPortal(col.gameObject, destination);
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        TryPortal(col.gameObject, destination);
+    }
+
+    private void TryPortal(GameObject target, Vector3 dest)
+    {
+        if (!LayerMaskExtensions.IsInLayerMask(target, _IgnoreTheseLayers))
+        {
+            PortalTo(target, dest);
+            if (stopRigidbody) SetRigidbodySpeed(target, Vector3.zero);
+        }
+    }
+
+    private void PortalTo(GameObject target, Vector3 dest)
+    {
+        target.transform.position = dest;
+    }
+
+    private void SetRigidbodySpeed(GameObject target, Vector3 speed)
+    {
+        Rigidbody rb = target.GetComponent<Rigidbody>();
+        Rigidbody2D rb2d = target.GetComponent<Rigidbody2D>();
+        if (rb == null && rb2d == null) return;
+        if (rb != null) rb.velocity = speed;
+        if (rb2d != null) rb2d.velocity = speed;
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/_PublicMethods/LayerMaskExtensions.cs
@@ -0,0 +1,110 @@
+﻿//LayerMaskExtensions from Unity Community Wiki: http://wiki.unity3d.com/index.php/LayerMaskExtensions
+//improved by STC
+//contact:          stc.ntu@gmail.com
+//last maintained:  2018/07/10
+
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace UnityExtension
+{
+    public static class LayerMaskExtensions
+    {
+        public static LayerMask Create(params string[] layerNames)
+        {
+            return NamesToMask(layerNames);
+        }
+
+        public static LayerMask Create(params int[] layerNumbers)
+        {
+            return LayerNumbersToMask(layerNumbers);
+        }
+
+        public static LayerMask NamesToMask(params string[] layerNames)
+        {
+            LayerMask ret = 0;
+            foreach (var name in layerNames)
+            {
+                ret |= (1 << LayerMask.NameToLayer(name));
+            }
+            return ret;
+        }
+
+        public static LayerMask LayerNumbersToMask(params int[] layerNumbers)
+        {
+            LayerMask ret = 0;
+            foreach (var layer in layerNumbers)
+            {
+                ret |= (1 << layer);
+            }
+            return ret;
+        }
+
+        public static LayerMask Inverse(this LayerMask original)
+        {
+            return ~original;
+        }
+
+        public static LayerMask AddToMask(this LayerMask original, params string[] layerNames)
+        {
+            return original | NamesToMask(layerNames);
+        }
+
+        public static LayerMask RemoveFromMask(this LayerMask original, params string[] layerNames)
+        {
+            LayerMask invertedOriginal = ~original;
+            return ~(invertedOriginal | NamesToMask(layerNames));
+        }
+
+        public static string[] MaskToNames(this LayerMask original)
+        {
+            var output = new List<string>();
+
+            for (int i = 0; i < 32; ++i)
+            {
+                int shifted = 1 << i;
+                if ((original & shifted) == shifted)
+                {
+                    string layerName = LayerMask.LayerToName(i);
+                    if (!string.IsNullOrEmpty(layerName))
+                    {
+                        output.Add(layerName);
+                    }
+                }
+            }
+            return output.ToArray();
+        }
+
+        public static string MaskToString(this LayerMask original)
+        {
+            return MaskToString(original, ", ");
+        }
+
+        public static string MaskToString(this LayerMask original, string delimiter)
+        {
+            return string.Join(delimiter, MaskToNames(original));
+        }
+
+        /// <summary>
+        /// Check if the target Layer is in the LayerMask.
+        /// </summary>
+        /// <param name="layer">The target layer. Try "gameobject.layer" </param>
+        /// <param name="layerMask">The layerMask. Consider using along with AddToMask() if you want to check multiple layer masks.</param>
+        /// <returns></returns>
+        public static bool IsInLayerMask(int layer, LayerMask layerMask)
+        {
+            return layerMask == (layerMask | (1 << layer));
+        }
+        /// <summary>
+        /// Check if the (layer of) target object is in the LayerMask.
+        /// </summary>
+        /// <param name="obj">The target gameobject.</param>
+        /// <param name="layerMask">The layerMask. Consider using along with AddToMask() if you want to check multiple layer masks.</param>
+        /// <returns></returns>
+        public static bool IsInLayerMask(GameObject obj, LayerMask layerMask)
+        {
+            return layerMask == (layerMask | (1 << obj.layer));
+        }
+
+    }
+}
