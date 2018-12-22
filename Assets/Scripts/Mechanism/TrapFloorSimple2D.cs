68dfd5b1ac70d6d8f4de307685adcc4759f2155b
samuelchou <samuelchou@hotmail.com.tw>
2018-12-22 17:57:00 +0800

    針對機關地板嘗試中....顯然是徒勞無功。但至少回復到原本的樣子了。

--- Assets/Scripts/Mechanism/TrapFloorSimple2D.cs
+++ Assets/Scripts/Mechanism/TrapFloorSimple2D.cs
@@ -1,6 +1,6 @@
 ﻿//TrapFloorSimple2D made by STC
 //contact:          stc.ntu@gmail.com
-//last maintained:  2018/10/31
+//last maintained:  2018/12/22
 //usage:            a basic trap floor, which you can set path and velocity.
 
 using UnityEngine;
@@ -47,14 +47,14 @@ public class TrapFloorSimple2D : Mechanism2D
         Vector2 jugdeD = JudgeMoveDirection();
         //if (direction == jugdeD) return;
         direction = jugdeD;
-        if (direction.magnitude != 0)
-        {
-            rb2d.bodyType = RigidbodyType2D.Dynamic;
-        }
-        else
-        {
-            rb2d.bodyType = RigidbodyType2D.Kinematic;
-        }
+        //if (direction.magnitude != 0)
+        //{
+        //    rb2d.bodyType = RigidbodyType2D.Dynamic;
+        //}
+        //else
+        //{
+        //    rb2d.bodyType = RigidbodyType2D.Kinematic;
+        //}
         rb2d.velocity = direction * speed;
 
     }
@@ -86,6 +86,13 @@ public class TrapFloorSimple2D : Mechanism2D
         }
         //since not activated nor need to return, no need to move
         return Vector2.zero;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        //TODO: 這邊要想辦法判定是不是可推動的物體
+        //DEV NOTE: OnCollisionEnter2D只會判定到有RB的物體...頂到地板或者無RB時就會失效。
+        //DebugMessage(LogType.Normal, col.contacts[0].ToString());
     }
 
 }
